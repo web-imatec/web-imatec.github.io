88e27fe8ee7d6526de16361e61de47acc18780b2
Matias Valdes <mvaldes@imatec.cl>
2016-07-16 22:17:37 -0400

    Change smooth-scroll.js fix url anchor on click animate

--- assets/js/smooth-scroll.js
+++ assets/js/smooth-scroll.js
@@ -2,11 +2,14 @@ $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
+      var hash = this.hash;
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
           scrollTop: target.offset().top
-        }, 1000);
+        }, 1000, function () {
+          location.href = hash;
+        });
         return false;
       }
     }
